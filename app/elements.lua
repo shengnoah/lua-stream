1b43504be8f13ef00049ebaba90fdd723b56f863
shengyang <tee168@qq.com>
2017-06-06 15:32:57 +0800

    去掉多余代码。

--- app/elements.lua
+++ app/elements.lua
@@ -1,17 +1,12 @@
 local pipeline = require "pipeline"
 
-local xss_plugin = require"plugin.content.xss_plugin"
-local sql_plugin = require"plugin.content.sql_plugin"
-local cc_plugin = require"plugin.content.cc_plugin"
-local httpsrc_plugin = require"plugin.content.httpsrc_plugin"
-local blockip_plugin = require"plugin.content.blockip_plugin"
-
 local status = pipeline:new {
-    httpsrc_plugin,
-    xss_plugin,
-    sql_plugin,
-    cc_plugin,
-    blockip_plugin,
+    xss_plugin = require"plugin.content.xss_plugin",
+    sql_plugin = require"plugin.content.sql_plugin",
+    cc_plugin = require"plugin.content.cc_plugin",
+    httpsrc_plugin = require"plugin.content.httpsrc_plugin",
+    blockip_plugin = require"plugin.content.blockip_plugin",
 }
+
 local ret = pipeline:run()
 
