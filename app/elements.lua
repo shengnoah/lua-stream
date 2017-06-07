e18924945845a842b5bbb1181849e469bd7f9889
shengyang <tee168@qq.com>
2017-06-07 13:39:15 +0800

    继续

--- app/elements.lua
+++ app/elements.lua
@@ -1,12 +1,10 @@
 local pipeline = require "pipeline"
-
 local status = pipeline:new {
-    xss_plugin = require"plugin.content.xss_plugin",
-    sql_plugin = require"plugin.content.sql_plugin",
-    cc_plugin = require"plugin.content.cc_plugin",
-    httpsrc_plugin = require"plugin.content.httpsrc_plugin",
-    blockip_plugin = require"plugin.content.blockip_plugin",
+    require"plugin.content.httpsrc_plugin",
+    require"plugin.content.blockip_plugin",
+    require"plugin.content.xss_plugin",
+    require"plugin.content.sql_plugin",
+    require"plugin.content.cc_plugin",
 }
-
 local ret = pipeline:run()
 
